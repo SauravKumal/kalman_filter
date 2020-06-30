e99c90194ad4d5cd05f23216c5f52a66865ddda4
Saurav Kumal <sk.sauravkumal@gmail.com>
2020-06-30 21:37:26 +0545

    fetch acceleration data from sensor

--- kalman_filter/kalman_filter.py
+++ kalman_filter/kalman_filter.py
@@ -1,4 +1,6 @@
 import socket
+import numpy as np
+import time
 UDP_IP = "192.168.1.83"
 UDP_PORT = 5555
 
@@ -6,4 +8,11 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 while True:
     data, addr = sock.recvfrom(4096)
-    print ("received message:", data)
+    data = data.decode('utf-8')
+    data = data.split(',')
+    data = np.array(data)
+    data = data.astype(np.float)
+    data = data[2:]
+    print(data)
+    #time.sleep(2)
+    
